d35287185d75485e9178bec027b845043fddbd8b
tupac34 <lesegomarimi99@gmail.com>
2025-01-24 12:09:37 +0200

    Removed backend files

--- backend/routes/products.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAllProducts, getProductById, addProduct } = require('../controllers/productController');
-
-router.get('/', getAllProducts);
-router.get('/:id', getProductById);
-router.post('/', addProduct);
-
-module.exports = router;
